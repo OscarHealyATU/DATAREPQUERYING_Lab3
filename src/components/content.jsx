0ab2c84a659a7352d6d934a6f0ff554805a6a097
martken <martin.kenirons@atu.ie>
2025-10-01 14:32:09 +0000

    Lab2 Complete

---
+++ src/components/content.jsx
@@ -0,0 +1,11 @@
+const Content = () => {
+
+    return (
+        <div>
+            <h1>Hello from Content Component!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+
+export default Content;
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <h1>Hello from Footer Component!</h1>
+            
+        </div>
+    );
+}
+
+export default Footer;
